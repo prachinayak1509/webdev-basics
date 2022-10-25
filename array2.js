da1a6f59abe9b10422e81235a6b3382f6c99176b
prachinayak1509 <prachinayak1509@gmail.com>
2022-10-25 23:13:04 +0530

    arrays 2

--- array2.js
+++ array2.js
@@ -2,3 +2,9 @@ var fruits=["mango","pear","apple","banana"];
 function loadFruits(){
     document.getElementById("fruits").innerHTML=fruits;
 }
+function myFunc(){
+    var fruit = prompt("what is your fav fruit");
+    fruits[fruits.length]=fruit;
+    document.getElementById("fruits").innerHTML=fruits;
+
+}
